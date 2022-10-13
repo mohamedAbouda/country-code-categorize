66f8d0db430b8321a040511f968c377d4873cd41
mohamed abouda <m.abouda@chefaa.com>
2022-10-13 05:17:06 +0200

    validate Morocco phone number

---
+++ app/Core/ValidateNumbers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Core;
+
+
+class ValidateNumbers
+{
+
+    public static function validatePhoneNumber($phone, $country): bool
+    {
+        $matches = [];
+
+        switch ($country) {
+            case 'Morocco':
+                preg_match('/^(?:(?:(?:\+|00)212[\s]?(?:[\s]?\(0\)[\s]?)?)|0){1}(?:5[\s.-]?[2-3]|6[\s.-]?[13-9]){1}[0-9]{1}(?:[\s.-]?\d{2}){3}$/', $phone, $matches, PREG_OFFSET_CAPTURE);
+                break;
+        }
+       
+        return count($matches) > 0;
+    }
+}
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Core\ValidateNumbers;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,15 +16,6 @@ class Customer extends Model
         '+212' => 'Morocco',
         '+258' => 'Mozambique',
         '+256' => 'Uganda',
-    ];
-
-
-    const COUNTRY_REGEX = [
-        'Cameroon'   => '\ (237\)\ ?[2368]\d{7,8}$',
-        'Ethiopia'   => '\ (251\)\ ?[1-59]\d{8}$',
-        'Morocco'    => '\ (212\)\ ?[5-9]\d{8}$',
-        'Mozambique' => '\ (258\)\ ?[28]\d{7,8}$',
-        'Uganda'     => '\ (256\)\ ?\d{9}$',
     ];
 
     protected $appends = ['code', 'country', 'state', 'phone_number'];
@@ -42,14 +34,13 @@ class Customer extends Model
 
     public function getPhoneNumberAttribute()
     {
-        $arr = preg_split('/\(.*?\)/',$this->phone);
+        $arr = preg_split('/\(.*?\)/', $this->phone);
 
         return trim(array_filter($arr)[1]);
     }
 
     public function getStateAttribute()
     {
-        preg_match('/^(?:(?:(?:\+|00)212[\s]?(?:[\s]?\(0\)[\s]?)?)|0){1}(?:5[\s.-]?[2-3]|6[\s.-]?[13-9]){1}[0-9]{1}(?:[\s.-]?\d{2}){3}$/',$this->code.$this->phone_number , $matches, PREG_OFFSET_CAPTURE);
-        return count($matches) > 0;
+        return ValidateNumbers::validatePhoneNumber($this->code . $this->phone_number, $this->country);
     }
 }
